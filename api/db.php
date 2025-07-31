5f62c8fb183f91383488b0eab18d5edd8bb08517
Braeden Treutel <braeden4328@gmail.com>
2025-07-31 14:26:59 -0500

    Changes for hosting

--- api/db.php
+++ api/db.php
@@ -1,15 +1,15 @@
 <?php
-// Database configuration
-$servername = "localhost"; // Use 'localhost' for XAMPP
-$username = "root";        // Default username for MySQL in XAMPP
-$password = "";            // Default password is empty for XAMPP
-$dbname = "ClassOverflow"; // Replace with your actual database name
+    // Database configuration
+    $servername = getenv('DB_HOST');
+    $username = getenv('DB_USER');
+    $password = getenv('DB_PASS');
+    $dbname = getenv('DB_NAME');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
