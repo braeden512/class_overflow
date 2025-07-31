05d7cc88c1ade62a7a70d41a23046c7fef23bdbb
Braeden Treutel <braeden4328@gmail.com>
2025-07-31 16:27:49 -0500

    CORS issues pt 9

--- api/register.php
+++ api/register.php
@@ -3,7 +3,7 @@
 // CORS headers
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
