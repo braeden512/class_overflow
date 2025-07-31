9aa1ec26640b4e8cfec079b3c517cff6bf4c21bf
Braeden Treutel <braeden4328@gmail.com>
2025-07-31 15:00:18 -0500

    CORS issues pt 2

--- api/register.php
+++ api/register.php
@@ -1,18 +1,18 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://classoverflow.vercel.app');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require 'db.php'; // Include the database connection
-
-// If it's a preflight request, return early
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit();
-}
 
 // Get the input data from the request body
 $data = json_decode(file_get_contents("php://input"), true);
